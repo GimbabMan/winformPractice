7032eba5339769a9cc68a2ac01a704c854de80e2
jongseok1234 <save930@likelion.org>
2020-05-11 19:29:51 +0900

    Add files via upload

---
+++ EMV_Project_ex3/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Reflection;
+
+namespace EMV_Project_ex3
+{
+    public partial class Form1 : Form
+    {
+        string sensorval;
+
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort1.PortName = "COM4";
+            serialPort1.BaudRate = 9600;
+            serialPort1.Open();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            sensorval = serialPort1.ReadLine();
+            this.Invoke(new EventHandler(DisplayText));
+        }
+        private void DisplayText(object sender, EventArgs e)
+        {
+            textBox1.AppendText(sensorval);
+        }
+
+    private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
